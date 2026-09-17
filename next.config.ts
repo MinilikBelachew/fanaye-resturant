0652364c317cd694b00458c0c2b7da4dfcdf4d86
aynuayex <aynuman19@gmail.com>
2026-09-17 12:36:38 +0300

    ci(deploy): switch frontend deployment to GitHub Packages (GHCR) with 15s VPS pull

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,6 @@ const nextConfig: NextConfig = {
     devIndicators: false,
 
     productionBrowserSourceMaps: false,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         ignoreBuildErrors: true,
     },
